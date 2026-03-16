6a199045251a52370fdd2c44c907836ff64789b0
claytojcms <joseclaytojcms@gmail.com>
2026-03-16 15:49:35 -0300

    Create Q6 LISTA.c
    É uma questão que pede para implementar a versão básica do algoritmo Selection Sort, realizando a ordenação de um vetor ao selecionar, a cada iteração, o menor elemento da parte não ordenada e colocá-lo na posição correta do vetor.

---
+++ Q6 LISTA.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void print_array(const int v[], int n) {
+    printf("[");
+    for (int i = 0; i < n; i++) {
+        printf("%d", v[i]);
+        if (i < n - 1) printf(", ");
+    }
+    printf("]");
+}
+
+static void swap_int(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void selectionSort(int v[], int n) {
+if( !v || n<=1)
+return;
+int comparacoes=0;
+int trocas=0;
+
+for(int i=0; i<n-1;i++){
+    int idx=i ;
+for(int j=i ;j<n ;j++){
+    if(v[j] < v[idx]){
+    comparacoes++;
+    idx=j;
+        }
+}
+     if (idx!=i){
+         trocas++;
+       swap_int(&v[i],&v[idx]);
+}
+}      printf("\ntrocas: %d comparacoes:%d\n",trocas,comparacoes);
+}
+
+int main() {
+
+    int dados[]  = {7, 9, 4, 2, 3};
+    int n = 5;
+    printf("VETOR ORIGINAL:\n");
+    print_array(dados,n);
+    selectionSort(dados,n);
+    printf("VETOR ORDENADO:\n");
+    print_array(dados,n);
+
+
+    return 0;
+}
