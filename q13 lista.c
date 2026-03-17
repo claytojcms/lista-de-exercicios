b055049a8b1a8ca9ab86099c2a385d3b54a40b36
claytojcms <joseclaytojcms@gmail.com>
2026-03-16 22:32:13 -0300

    Create q13 lista.c
    É uma questão que pede para adicionar métricas de comparações e movimentações no algoritmo Insertion Sort, incrementando esses valores durante a execução para acompanhar quantas operações são realizadas no processo de ordenação.

---
+++ q13 lista.c
@@ -0,0 +1,64 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+
+void imprimir_vetor(int V[],int n){
+ for(int i = 0;i < n; i++)
+    printf(" %d",V[i]);
+    printf("\n");
+
+ }
+
+    void insertionSort(int V[], int n) {
+int i, j;
+int chave;
+int comparacoes=0;
+int movimentacoes=0;
+for (i = 1; i < n; i++) {
+
+chave = V[i];
+j = i;
+
+while (j > 0 && chave < V[j - 1]) {
+    comparacoes++;
+    V[j] = V[j - 1];
+    j--;
+    movimentacoes++;
+}
+    comparacoes++;
+    V[j] = chave;
+}
+printf("movimentacoes:%d\n  comparacoes:%d\n",movimentacoes,comparacoes);
+}
+
+
+
+    int main()
+    {
+
+        int n=20;
+      int A[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+      int B[]={20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+      int C[]={54,76,10,65,8,90,43,81,13,25,67,40,7,14,38,51,18,2,45,20};
+
+        printf("VETOR A: Antes \n");
+        imprimir_vetor(A,n);
+        printf("DEPOIS:\n");
+        insertionSort(A,n);
+        imprimir_vetor(A,n);
+
+        printf("VETOR B: ANTES \n");
+        imprimir_vetor(B,n);
+        printf("DEPOIS: \n");
+        insertionSort(B,n);
+        imprimir_vetor(B,n);
+
+        printf("VETOR C: ANTES \n");
+        imprimir_vetor(C,n);
+        printf("DEPOIS: \n");
+        insertionSort(C,n);
+        imprimir_vetor(C,n);
+
+
+
+        return 0;
+    }
