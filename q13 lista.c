d867f6d7fe7093d5a60300f542c03da84ed6b1bc
claytojcms <joseclaytojcms@gmail.com>
2026-03-16 22:24:56 -0300

    Create q13 lista.c
    É uma questão que pede para mostrar o vetor ao final de cada iteração do laço for no Insertion Sort, permitindo acompanhar como o vetor vai sendo gradualmente ordenado a cada passo do algoritmo.

---
+++ q13 lista.c
@@ -0,0 +1,46 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+
+void imprimir_vetor(int V[],int n){
+ for(int i = 0;i < n; i++)
+    printf(" %d",V[i]);
+    printf("\n");
+
+ }
+
+    void insertionSort(int V[], int n) {
+int i, j;
+int chave;
+for (i = 1; i < n; i++) {
+chave = V[i];
+j = i;
+
+while (j > 0 && chave < V[j - 1]) {
+    V[j] = V[j - 1];
+    j--;
+
+}
+    V[j] = chave;
+    printf("estado apos a iteracao:");
+    imprimir_vetor(V,n);
+}
+}
+
+
+
+    int main()
+    {
+
+        int n=5;
+        int A[]={9,6,1,4,7};
+
+        printf("VETOR A: Antes \n");
+        imprimir_vetor(A,n);
+        insertionSort(A,n);
+        printf("ESTADO FINAL:\n");
+        imprimir_vetor(A,n);
+
+
+
+        return 0;
+    }
