3157bf42e0f6cf34ee5341e9f91d96758f8872a7
claytojcms <joseclaytojcms@gmail.com>
2026-03-24 15:25:17 -0300

    Update q1 lista.c
    É uma questão que pede para implementar a versão básica do algoritmo Bubble Sort, realizando a ordenação de um vetor por meio da comparação e troca de elementos adjacentes, até que todo o vetor esteja ordenado.

--- q1 lista.c
+++ q1 lista.c
@@ -4,7 +4,7 @@
 
 void bubbleSort(int v[], int n) {
     for(int i=0; i< n ;i++){
-        for(int j=0 ; j<n-1 ; j++){
+        for(int j=0 ; j<n-i-1 ; j++){
             if(v[j]>v[j+1]){
                 int temp= v[j+1];
                 v[j+1]= v[j];
